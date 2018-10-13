6de38103d62225c49a4b60b943d3d3b82728f6e7
omrobbie <putu.roby@yahoo.com>
2018-10-13 20:19:08 +0700

    setup template fragment to all item on bottom navigation view

--- app/src/main/java/com/omrobbie/footballapps/main/MainActivity.kt
+++ app/src/main/java/com/omrobbie/footballapps/main/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.omrobbie.footballapps
+package com.omrobbie.footballapps.main
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import com.omrobbie.footballapps.R
+import com.omrobbie.footballapps.favorites.FavoritesFragment
+import com.omrobbie.footballapps.matches.MatchesFragment
+import com.omrobbie.footballapps.teams.TeamsFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,26 +20,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupEnv() {
+        setFragment(MatchesFragment())
+        listenBottomNavigationView()
+    }
+
+    private fun listenBottomNavigationView() {
         bottom_navigation_view.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 bottom_navigation_view.selectedItemId -> return@setOnNavigationItemSelectedListener false
 
                 R.id.bnv_matches -> {
-                    toast("matches")
+                    setFragment(MatchesFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
 
                 R.id.bnv_teams -> {
-                    toast("teams")
+                    setFragment(TeamsFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
 
                 R.id.bnv_favorites -> {
-                    toast("favorites")
+                    setFragment(FavoritesFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
             }
             return@setOnNavigationItemSelectedListener false
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        with(supportFragmentManager.beginTransaction()) {
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            replace(R.id.frame_layout, fragment)
+            commit()
         }
     }
 }
---
+++ app/src/main/java/com/omrobbie/footballapps/matches/MatchesFragment.kt
@@ -0,0 +1,16 @@
+package com.omrobbie.footballapps.matches
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.omrobbie.footballapps.R
+
+class MatchesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_matches, container, false)
+    }
+}
