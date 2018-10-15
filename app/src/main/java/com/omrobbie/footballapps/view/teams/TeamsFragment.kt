0df8ce6a7933b9b95b17a5b8abb0ef5e488a216c
omrobbie <putu.roby@yahoo.com>
2018-10-15 22:46:08 +0700

    setup layout for team list

--- app/src/main/java/com/omrobbie/footballapps/view/teams/TeamsFragment.kt
+++ app/src/main/java/com/omrobbie/footballapps/view/teams/TeamsFragment.kt
@@ -3,11 +3,10 @@ package com.omrobbie.footballapps.view.teams
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 
 import com.omrobbie.footballapps.R
+import com.omrobbie.footballapps.utils.loadFirstText
 
 import kotlinx.android.synthetic.main.fragment_teams.*
 
@@ -27,5 +26,7 @@ class TeamsFragment : Fragment() {
         with(activity as AppCompatActivity) {
             setSupportActionBar(toolbar)
         }
+
+        spinner.loadFirstText(context!!)
     }
 }
