ffc2af67d6f881dfb42891782c2bdeb3c8034b70
omrobbie <putu.roby@yahoo.com>
2018-10-15 22:57:00 +0700

    setup search view for team list

--- app/src/main/java/com/omrobbie/footballapps/view/teams/TeamsFragment.kt
+++ app/src/main/java/com/omrobbie/footballapps/view/teams/TeamsFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.*
+import android.widget.SearchView
 
 import com.omrobbie.footballapps.R
+import com.omrobbie.footballapps.utils.invisible
 import com.omrobbie.footballapps.utils.loadFirstText
+import com.omrobbie.footballapps.utils.visible
 
 import kotlinx.android.synthetic.main.fragment_teams.*
+import org.jetbrains.anko.support.v4.toast
 
 class TeamsFragment : Fragment() {
 
@@ -22,11 +26,41 @@ class TeamsFragment : Fragment() {
         setupEnv()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater?.inflate(R.menu.menu_search_view, menu)
+
+        val searchMenu = menu?.findItem(R.id.mn_search_view)
+        val searchView = searchMenu?.actionView as SearchView
+
+        listenSearchView(searchView)
+    }
+
     private fun setupEnv() {
+        setHasOptionsMenu(true)
+
         with(activity as AppCompatActivity) {
             setSupportActionBar(toolbar)
         }
 
         spinner.loadFirstText(context!!)
+    }
+
+    private fun listenSearchView(searchView: SearchView) {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                toast(query.toString())
+
+                return true
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                if (query.toString().isEmpty()) spinner_container.visible()
+                else spinner_container.invisible()
+
+                return false
+            }
+        })
     }
 }
